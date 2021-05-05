f48ffec8729ae0fae427cdfc588794536a825d22
jangyejoo <jyj01068662778@gmail.com>
2021-05-06 01:21:06 +0900

    css, js 적용

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ var db = require('./router/db');
 var sanitizeHtml = require('sanitize-html');
 
 app.use(express.static('public'));
+app.use(express.static('router'));
 
 app.get('/signUp', function(request, response){
-        response.sendfile(__dirname + './views/signUp.html');
+        response.sendfile(__dirname + '/views/signUp.html');
+});
+
+app.get('/add_product_admin', function(request, response){
+        response.sendfile(__dirname + '/views/add_product_admin.html');
+});
+
+app.get('/product_list_admin', function(request, response){
+        response.sendfile(__dirname + '/views/product_list_admin.html');
+});
+
+app.get('/product_list', function(request, response){
+        response.sendfile(__dirname + '/views/product_list.html');
 });
 
 app.listen(3000, function(){
