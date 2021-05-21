faa78bbb8ed5eff2a442de557e7aaa7962393e0b
byein <kyebin0825@gmail.com>
2021-05-21 22:53:56 +0900

    등록 페이지 등록 날짜 제거, 상품코드 제거, 멀티파일 업로드 가능하도록  변경

--- router/add_banner_admin.js
+++ router/add_banner_admin.js
@@ -5,8 +5,6 @@ function check() {
         return false;
     } else if (!banner_period_start_check()) {
         return false;
-    } else if (!banner_reg_date_check()) {
-        return false;
     } else if (!banner_img_check()) {
         return false;
     } else {
@@ -100,40 +98,6 @@ function banner_period_start_check() {
         return false;
     } else if (year_start > year_end || month_start > month_end || date_start > date_end) {
         alert("시작 날짜가 종료 날짜보다 더 늦습니다. 시작 날짜와 종료 날짜가 유효한지 다시 확인해 주세요.");
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-
-/*기타 설명은 옵션으로 유효성 검사 제외*/
-function banner_reg_date_check() {
-    var banner_reg_date = document.getElementById("banner_reg_date");
-
-    banner_reg_date = new Date(banner_reg_date.value);
-    var year = banner_reg_date.getFullYear();
-    var month = banner_reg_date.getMonth() + 1;
-    var date = banner_reg_date.getDate();
-
-    if (isNaN(year) || isNaN(month) || isNaN(date)) {
-        year = 0;
-        month = 0;
-        date = 0;
-    }
-
-    var today = new Date();
-    var todayYear = today.getFullYear();
-    var todayMonth = today.getMonth() + 1;
-    var todayDate = today.getDate();
-
-    if (year==0&&month==0&date==0) {
-        alert("등록 날짜를 입력해 주세요.");
-        return false;
-    } else if (todayYear != year || todayMonth != month || todayDate != date) {
-        alert("등록 날짜는 오늘만 가능합니다.");
-        banner_period_start.value == "";
         return false;
     } else {
         return true;
