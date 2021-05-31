d26be16e57f99a1e3d17e3ac8685f2e2b9031e04
Jung Hyeon <dlqjdgus99@naver.com>
2021-06-01 02:25:03 +0900

    docs: Add date validation check fuction

--- router/inquiry_admin.js
+++ router/inquiry_admin.js
@@ -16,11 +16,18 @@ function saveAnswer() {
 }
 
 
-// 미완성
+// 간단한 유효성 검사
 function onDateCheck() {
     const from_date = new Date(document.getElementById('search-date-from').value);
     const to_date = new Date(document.getElementById('search-date-to').value);
     const today = new Date();
 
+    if(to_date===NULL || to_date > today) to_date = today;      // 끝 날짜가 설정되어있지 않거나 today보다 뒤인 끝날짜이면 today로 간주한다.
+    if(from_date==NULL) from_date = today;                      // 시작날짜가 설정되어있지 않으면 today로 간주한다.
 
+    if(from_date > to_date) {                   // 시작날짜가 끝날짜보다 더 뒤에 있으면 
+        alert('기간 설정을 다시 해주세요.');
+        return false; 
+    }
+    else return true;
 }
