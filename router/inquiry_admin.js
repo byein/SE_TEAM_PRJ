e9a61600e7568e6a560b1279927fcae4a30123c7
JHyeon0915 <dlqjdgus99@naver.com>
2021-06-08 17:50:30 +0900

    fix: Modify css

--- router/inquiry_admin.js
+++ router/inquiry_admin.js
@@ -1,8 +1,8 @@
 function showDetails(index) {
-    const lNum = 'inquiry-details'+index;
-    if(document.getElementById(lNum).style.display === 'table-row')
-        document.getElementById(lNum).style.display = 'none';
-    else document.getElementById(lNum).style.display = 'table-row';
+    const lNum = index;
+    if(document.getElementById('inquiry-details'+lNum).style.display === 'table-row')
+        document.getElementById('inquiry-details'+lNum).style.display = 'none';
+    else document.getElementById('inquiry-details'+lNum).style.display = 'table-row';
 }
 
 function saveAnswer() {
@@ -14,21 +14,4 @@ function saveAnswer() {
         return true;
     }
     else return false;
-}
-
-
-// 간단한 유효성 검사
-function onDateCheck() {
-    const from_date = new Date(document.getElementById('search-date-from').value);
-    const to_date = new Date(document.getElementById('search-date-to').value);
-    const today = new Date();
-
-    if(to_date===NULL || to_date > today) to_date = today;      // 끝 날짜가 설정되어있지 않거나 today보다 뒤인 끝날짜이면 today로 간주한다.
-    if(from_date==NULL) from_date = today;                      // 시작날짜가 설정되어있지 않으면 today로 간주한다.
-
-    if(from_date > to_date) {                   // 시작날짜가 끝날짜보다 더 뒤에 있으면 
-        alert('기간 설정을 다시 해주세요.');
-        return false; 
-    }
-    else return true;
 }
